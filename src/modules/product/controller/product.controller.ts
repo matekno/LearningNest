22e2361ae1b9e1ebef4d867d705e92971cb19e5a
matekno <mati.rapoport@gmail.com>
2022-11-04 16:23:13 -0300

    wip: learning post

--- src/modules/product/controller/product.controller.ts
+++ src/modules/product/controller/product.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Res, Response, Request, Body, HttpStatus } from '@nestjs/common';
 
 @Controller('product')
 export class ProductController {
   @Get('/')
   sayHello() {
     return 'Hello from product controller!!'
+  }
+
+  @Post()
+  postProducto(@Res() res: Response, @Body() payload: any) {
+    return res.ok().json({
+      msg: 'Add a new product',
+      payload,
+    });
   }
 }
